91fea0917c74df4b9827cb4efa9c1d4b0d1d2cb0
camber boles <theaffablealmond@gmail.com>
2023-07-09 11:04:51 -0400

    avocado animations, menus, scripts

--- GameJam23/Assets/Scripts/SettingsMenu.cs
+++ GameJam23/Assets/Scripts/SettingsMenu.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class SettingsMenu : MonoBehaviour
@@ -9,24 +12,89 @@ public class SettingsMenu : MonoBehaviour
     [Header("Volume Configuration")]
     [SerializeField]
     Slider volumeSlider;
+    float currentVolume = 0.5f;
 
     [Header("Graphics Configuration")]
     [SerializeField]
     TMP_Dropdown graphicsDropdown;
+    List<string> graphicsOptions = new List<string>();
 
     [Header("Resolution Configuration")]
     [SerializeField]
     TMP_Dropdown resDropdown;
+    Resolution[] resolutions;
+    List<string> resOptions = new List<string>();
+    bool isFullScreen;
+
+    EventSystem eventSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set up the computer's available graphics levels in menu
+        graphicsDropdown.ClearOptions();
+        for (int i = 0; i < QualitySettings.names.Length; i++)
+        {
+            graphicsOptions.Add(QualitySettings.names[i]);
+        }
+        graphicsDropdown.AddOptions(graphicsOptions);
+
+        // Set up the computer's available resolutions in the menu
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            resOptions.Add(resolutions[i].ToString());
+        }
+        resDropdown.AddOptions(resOptions);
+
+        isFullScreen = Screen.fullScreen;
+
+        // Get the event system
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        eventSystem.SetSelectedGameObject(volumeSlider.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    // When the menu is enabled
+    void OnEnable()
     {
-        
+        // currentVolume gets set to whatever music player is
+        volumeSlider.SetValueWithoutNotify(currentVolume);
+
+        int graphicsIndex = QualitySettings.GetQualityLevel();
+        if (graphicsDropdown)
+        {
+            graphicsDropdown.SetValueWithoutNotify(graphicsIndex);
+        }
+
+        if (resolutions != null)
+        {
+            int resIndex = Array.IndexOf(resolutions,
+                Screen.currentResolution.ToString());
+            if (resDropdown)
+            {
+                resDropdown.SetValueWithoutNotify(resIndex);
+            }
+        }
+    }
+
+    // Changing Volume
+    public void VolumeChange()
+    {
+        currentVolume = volumeSlider.value;
+    }
+
+    // Changing Graphics
+    public void GraphicsChange()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+
+    // Changing Resolution
+    public void ResChange()
+    {
+        Screen.SetResolution(resolutions[resDropdown.value].width,
+                             resolutions[resDropdown.value].height,
+                             isFullScreen);
     }
 }
